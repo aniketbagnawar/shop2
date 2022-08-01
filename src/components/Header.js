115f925c7675ce1c4dc7602bcd978b5eeb72e6d5
bhushan9834 <55429915+bhushan9834@users.noreply.github.com>
2022-08-01 19:52:08 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,6 +19,9 @@ const Header = () => {
                     <li>
                         <Link to="/checkout">Checkout</Link>
                     </li>
+                    <li>
+                        <Link to="/admin">Admin</Link>
+                    </li>
                 </ul>
             </nav>
         </header>
